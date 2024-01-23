5762ffa4fd170fe4ac18018774b2a9a764c3e6cd
Ashfaq Shimer <shimer.ahamed@gmail.com>
2024-01-24 02:12:00 +0530

    Populate message related data. Limit messages. Develop load more endpoint

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -7,22 +7,30 @@ import ErrorResponse from '../utils/ErrorResponse.js';
 // @route   GET /api/v1/chats
 // @access  Private
 export const getUserConversations = asyncHandler(async (req, res, next) => {
-	try {
-		const userId = req.user._id;
+	const userId = req.user._id;
 
-		const conversations = await Conversation.find({
-			users: userId,
-		}).populate('users', 'firstName lastName');
+	const conversations = await Conversation.find({
+		users: userId,
+	})
+		.populate('users', 'firstName lastName')
+		.populate({
+			path: 'messages',
+			select: 'content sender timestamp',
+			populate: {
+				path: 'sender',
+				select: 'firstName lastName',
+			},
+			options: {
+				sort: { timestamp: -1 },
+				limit: process.env.CONVERSATION_MESSAGE_LIMIT || 50,
+			},
+		});
 
-		res.status(200).json({ success: true, data: conversations });
-	} catch (error) {
-		console.error(error);
-		return next(new ErrorResponse('Could not fetch user conversations', 500));
-	}
+	res.status(200).json({ success: true, data: conversations });
 });
 
 // @desc   Create a new conversation or return a Conversation based on participants
-// @route   POST /api/v1/chats/conversation
+// @route   POST /api/v1/chats/
 // @access  Private
 export const findOrCreateChat = asyncHandler(async (req, res, next) => {
 	const userId = req.user._id;
@@ -43,6 +51,56 @@ export const findOrCreateChat = asyncHandler(async (req, res, next) => {
 		const newConversation = await Conversation.create({ users });
 		res.status(201).json({ success: true, data: newConversation });
 	}
+});
+
+// @desc   Load more messages for a conversation
+// @route   GET /api/v1/chats/:conversationId/messages
+// @access  Private
+export const loadMoreMessages = asyncHandler(async (req, res, next) => {
+	const { conversationId } = req.params;
+	const { page = 2, limit = process.env.CONVERSATION_MESSAGE_LIMIT || 50 } =
+		req.query;
+
+	// Validate if the user is part of the conversation
+	const isUserInConversation = await Conversation.exists({
+		_id: conversationId,
+		users: req.user._id,
+	});
+
+	if (!isUserInConversation) {
+		return next(
+			new ErrorResponse(
+				'User is not authorized to access messages in this conversation',
+				403
+			)
+		);
+	}
+
+	// Fetch messages for the specified page and limit
+	const conversation = await Conversation.findById(conversationId).populate({
+		path: 'messages',
+		select: 'content sender timestamp',
+		populate: {
+			path: 'sender',
+			select: 'firstName lastName',
+		},
+		options: {
+			sort: { timestamp: -1 },
+			skip: (page - 1) * limit,
+			limit: +limit,
+		},
+	});
+
+	if (!conversation) {
+		return next(
+			new ErrorResponse(
+				'Conversation not found or user is not a participant',
+				404
+			)
+		);
+	}
+
+	res.status(200).json({ success: true, data: conversation.messages });
 });
 
 // @desc   Create a new message in a conversation
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -4,12 +4,15 @@ import {
 	createMessage,
 	findOrCreateChat,
 	getUserConversations,
+	loadMoreMessages,
 } from '../controllers/chatController.js';
 
 const router = express.Router();
 
-router.get('/', protect, getUserConversations);
-router.post('/conversation', protect, findOrCreateChat);
-router.post('/message', protect, createMessage);
+router.use(protect);
+
+router.get('/', getUserConversations).post('/', findOrCreateChat);
+router.get('/:conversationId', loadMoreMessages);
+router.post('/message', createMessage);
 
 export default router;
