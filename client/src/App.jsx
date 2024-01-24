1aae32398381a43378b341540ff777464607c92c
Ashfaq Shimer <shimer.ahamed@gmail.com>
2024-01-25 02:26:17 +0530

    Develop basic navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './common/Navbar';
 
 const App = () => {
 	return (
 		<>
 			{/* Add navbar here */}
+			<Navbar/>
 			<Outlet />
 		</>
 	);
---
+++ client/src/common/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	InputBase,
+	IconButton,
+	Button,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+	Search,
+	Message,
+	DarkMode,
+	LightMode,
+	Notifications,
+	Help,
+	Menu,
+	Close,
+} from '@mui/icons-material';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
+const Navbar = () => {
+	return (
+		<AppBar position='static' sx={{ backgroundColor: '#FA991C' }}>
+			<Toolbar>
+				{/* Logo and Search */}
+				<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+					Your Logo
+				</Typography>
+				<div style={{ display: 'flex', alignItems: 'center' }}>
+					<div style={{ marginRight: '16px' }}>
+						<IconButton color='inherit'>
+							<SearchIcon />
+						</IconButton>
+					</div>
+					<InputBase
+						placeholder='Search...'
+						inputProps={{ 'aria-label': 'search' }}
+						sx={{
+							backgroundColor: 'white',
+							borderRadius: '4px',
+							padding: '8px',
+						}}
+					/>
+				</div>
+
+				{/* Icons on the right */}
+				<IconButton color='inherit'>
+					<DarkMode />
+				</IconButton>
+				<IconButton color='inherit'>
+					<Message />
+				</IconButton>
+				<IconButton color='inherit'>
+					<Notifications />
+				</IconButton>
+
+				{/* Logout Button */}
+				<Button color='inherit' startIcon={<ExitToAppIcon />}>
+					Logout
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export default Navbar;
