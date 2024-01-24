3b7d0ee77777a57f4b4e10d2ea4e568e326027e5
Ashfaq Shimer <shimer.ahamed@gmail.com>
2024-01-24 16:28:35 +0530

    Initialize frontend with vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App.jsx';
+import './index.css';
+import store from './store';
+import LoginPage from './pages/LoginPage.jsx';
+
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index={true} path='/' element={<div>Home</div>} />
+			<Route path='/login' element={<LoginPage />} />
+		</Route>
+	)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<Provider store={store}>
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+	</Provider>
+);
